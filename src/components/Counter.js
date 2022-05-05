933873589639a179de7a1c8b5cbbb91d61fd107c
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-05-05 10:45:03 +0200

    redux store and provider setup

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './Counter.module.css';
+
+const Counter = () => {
+    return <div className={styles.counter}>
+        <h1>Counter</h1>
+    </div>
+};
+
+export default Counter;
