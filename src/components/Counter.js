9c0b8970b0a23ce7fb59f2320bc64b0b27556df2
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-05-05 12:28:18 +0200

    useDispatch and useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
 import styles from './Counter.module.css';
 
 const Counter = () => {
+    const [counterValue, showCounter] = useSelector(state => [state.counter, state.showCounter])
+    const dispatch = useDispatch()
+
+    const incrementHandler = () => {
+        dispatch({ type: 'increment' })
+    }
+
+    const decrementHandler = () => {
+        dispatch({ type: 'decrement' })
+    }
+
+    const toggleCounterHandler = () => {
+        dispatch({ type: 'toggle'})
+    }
+
     return <div className={styles.counter}>
-        <h1>Counter</h1>
+        <h1>Redux Counter</h1>
+        {showCounter && <div className={styles.value}>{counterValue}</div>}
+        <div className={styles.actions}>
+            <button onClick={incrementHandler}>+</button>
+            <button onClick={decrementHandler}>-</button>
+        </div>
+        <button onClick={toggleCounterHandler}>Toggle counter</button>
     </div>
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { legacy_createStore as createStore } from 'redux';
 
-const counterReducer = (state = { counter : 0 }, action) => {
-    if (action.type === 'increment') return { counter: state.counter + 1 };
-    if (action.type === 'decrement') return { counter: state.counter - 1 };
-    return state;
+const initialState = {
+    counter: 0,
+    showCounter: true,
+}
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'increment': return { ...state, counter: state.counter + 1 };
+        case 'decrement': return { ...state, counter: state.counter - 1 };
+        case 'toggle': return { ...state, showCounter: !state.showCounter };
+        default: return state;
+    }
 }
 
 const store = createStore(counterReducer);
